ad5edcba80594259a586ac54ce80e4b3cc6fa046
mahmoudreza <m.r.nateghi12@gmail.com>
2022-09-29 14:49:32 +0330

    color-query

--- queryselector.js
+++ queryselector.js
@@ -1,3 +1,11 @@
 document.querySelector('#qu1').addEventListener('input',(e)=>{
     console.log(e.target.value)
+    if(e.target.value =='45442'){
+        console.log('recapture is true')
+        document.querySelector('#qu2').style.backgroundColor= 'green'
+    }
+    else{
+        console.log('recapture is false')
+        document.querySelector('#qu2').style.backgroundColor='red'
+    }
 })
